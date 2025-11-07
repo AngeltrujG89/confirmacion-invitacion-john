84fa97704e64004171708d313ad4fa471eabf397
AngelTrujG89 <latg8904@gmail.com>
2025-11-06 21:09:15 -0600

    integreacion firebase con angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { environment } from '../enviroments/enviroment';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
